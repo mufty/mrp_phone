e29cc9350dab31965310fba39d4812e8ba479e03
gubriansky@sernato.sk <gubriansky@sernato.sk>
2021-06-21 09:37:43 +0200

    added logs

--- html/apps/notifications/scripts/notifications.js
+++ html/apps/notifications/scripts/notifications.js
@@ -60,7 +60,6 @@ class Notifications {
         }
 
         if (this.pendingNotifications[id]) {
-            console.log("this.pendingNotifications[id] .. remove");
             this.removeNotification(id);
         }
 
@@ -72,7 +71,6 @@ class Notifications {
             top: '0px'
         }, this.cfg.animationLength);
         notification.click(() => {
-            console.log("notification.click .. remove");
             this.removeNotification(id);
         });
 
@@ -81,7 +79,6 @@ class Notifications {
         if (!sticky) {
             //delete notification after some time
             setTimeout(() => {
-                console.log("hideNonStickyNotification .. remove");
                 this.hideNonStickyNotification(id);
             }, this.cfg.notificationLifetime);
         }
